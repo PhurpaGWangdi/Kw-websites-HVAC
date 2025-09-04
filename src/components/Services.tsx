d4f8f43513e8634cacfafb7c18d1a3787ec9b440
PhurpaGWangdi <coolaw1997@gmail.com>
2025-09-04 11:33:49 +0300

    Add AVATARS map for testimonial images

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -38,6 +38,13 @@ const IMAGES = {
   commercial: "/images/commercial.jpg",
 };
 
+const AVATARS = {
+  maha: "/avatars/maha.jpg",
+  yousef: "/avatars/yousef.jpg",
+  noura: "/avatars/noura.jpg",
+  ahmad: "/avatars/ahmad.jpg",
+};
+
 const Services: React.FC = () => {
   const { language, isRTL } = useLanguage();
   const copy = COPY[language];
